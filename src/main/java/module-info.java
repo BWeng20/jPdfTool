9a493809fe7230eea59a0a904e87192b541b6889
Bernd <Bernd.Wengenroth@gmail.com>
2025-09-10 15:30:43 +0200

    Added distribution with jre included.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module jPdfTool.main {
+    requires java.base;
+    requires java.desktop;
+    requires java.prefs;
+    requires org.apache.pdfbox;
+    requires com.formdev.flatlaf;
+    exports com.bw.jPdfTool;
+}
