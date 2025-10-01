4cc4746b04f4ff90535048453a33d4c9cbc4c6c6
Bernd <Bernd.Wengenroth@gmail.com>
2025-10-01 16:23:01 +0200

    Loads arguments as pdf files.

--- src/main/java/com/bw/jPdfTool/Main.java
+++ src/main/java/com/bw/jPdfTool/Main.java
@@ -1,19 +1,37 @@
 package com.bw.jPdfTool;
 
+import com.bw.jPdfTool.model.MergeOptions;
 import com.bw.jtools.svg.SVGConverter;
 import com.bw.jtools.ui.ShapeMultiResolutionImage;
 import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
 import com.formdev.flatlaf.util.SystemInfo;
 
 import javax.swing.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
+
+        main.files = new ArrayList<>(args.length);
+        for (String a : args) {
+            Path p = Paths.get(a);
+            if (Files.exists(p))
+                main.files.add(p);
+            else {
+                Log.error("File '%s' doesn't exists.", a);
+            }
+        }
+
         SwingUtilities.invokeLater(main::createUI);
     }
 
+    private List<Path> files;
 
     protected void createUI() {
 
@@ -47,8 +65,14 @@ public class Main {
         mainWindow.setLocationByPlatform(true);
         mainWindow.setContentPane(ui);
         mainWindow.pack();
-
         mainWindow.setVisible(true);
+
+        if (!files.isEmpty()) {
+            MergeOptions mo = new MergeOptions();
+            ui.selectPdf(files.get(0).toFile());
+            for (int i = 1; i < files.size(); ++i)
+                ui.appendPdf(files.get(i).toFile(), mo);
+        }
     }
 
     public static JFrame mainWindow;
